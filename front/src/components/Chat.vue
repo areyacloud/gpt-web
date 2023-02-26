f4a36feec6dfee244dcde4e67980437f995589bc
areyacloud <thirdtrick@163.com>
2023-02-26 09:33:03 +0800

    [init]first commit

--- front/src/components/Chat.vue
+++ front/src/components/Chat.vue
@@ -7,7 +7,7 @@
 .box-card {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   font-weight: bolder;
-    max-width: 50rem;
+  max-width: 50rem;
   margin: auto;
   color: #f4f4f5;
   background-color: #30b987a1;
@@ -15,7 +15,7 @@
 }
 
 .send-btn {
-  color : #ffffff;
+  color: #ffffff;
   margin: auto;
   width: 100%;
 }
@@ -27,7 +27,7 @@
 .chat-card {
   width: auto;
   max-width: 50rem;
-  margin:auto;
+  margin: auto;
   height: 100%;
   overflow-y: auto;
   background-color: #f4f4f5;
@@ -35,8 +35,8 @@
 
 .msg-form {
   width: auto;
-    max-width: 50rem;
-    margin: auto;
+  max-width: 50rem;
+  margin: auto;
 }
 
 .chat-input {
@@ -51,12 +51,12 @@ body,
 .el-container {
   height: 100%;
   overflow-y: hidden;
-    overflow-x: hidden;
-  background-color:#ffffff;
+  overflow-x: hidden;
+  background-color: #ffffff;
 }
 
 .el-main {
-    overflow-x: hidden;
+  overflow-x: hidden;
   overflow-y: hidden;
 }
 
@@ -69,7 +69,7 @@ body,
   border-radius: 3px;
   width: fit-content;
   line-height: normal;
-  background-color:#30b987a1;
+  background-color: #30b987a1;
   font-size: small;
 }
 
@@ -80,137 +80,111 @@ body,
   background-color: #ffffff;
   font-size: small;
 }
-
 </style>
 
 <template>
   <div class="common-layout">
     <el-container>
-      <!-- <el-aside width="300px">
-              <el-scrollbar>
-                <el-menu :default-openeds="['1', '3']">
-                  <el-sub-menu index="1">
-<template #title>
-  <el-icon>
-    <message />
-  </el-icon>Navigator One
-</template>
-<el-menu-item-group>
-  <template #title>Group 1</template>
-  <el-menu-item index="1-1">Option 1</el-menu-item>
-  <el-menu-item index="1-2">Option 2</el-menu-item>
-</el-menu-item-group>
-<el-menu-item-group title="Group 2">
-  <el-menu-item index="1-3">Option 3</el-menu-item>
-</el-menu-item-group>
-<el-sub-menu index="1-4">
-  <template #title>Option4</template>
-  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-</el-sub-menu>
-                  </el-sub-menu>
-                  <el-sub-menu index="2">
-<template #title>
-  <el-icon><icon-menu /></el-icon>Navigator Two
-</template>
-<el-menu-item-group>
-  <template #title>Group 1</template>
-  <el-menu-item index="2-1">Option 1</el-menu-item>
-  <el-menu-item index="2-2">Option 2</el-menu-item>
-</el-menu-item-group>
-<el-menu-item-group title="Group 2">
-  <el-menu-item index="2-3">Option 3</el-menu-item>
-</el-menu-item-group>
-<el-sub-menu index="2-4">
-  <template #title>Option 4</template>
-  <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-</el-sub-menu>
-                  </el-sub-menu>
-                  <el-sub-menu index="3">
-<template #title>
-  <el-icon>
-    <setting />
-  </el-icon>Navigator Three
-</template>
-<el-menu-item-group>
-  <template #title>Group 1</template>
-  <el-menu-item index="3-1">Option 1</el-menu-item>
-  <el-menu-item index="3-2">Option 2</el-menu-item>
-</el-menu-item-group>
-<el-menu-item-group title="Group 2">
-  <el-menu-item index="3-3">Option 3</el-menu-item>
-</el-menu-item-group>
-<el-sub-menu index="3-4">
-  <template #title>Option 4</template>
-  <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-</el-sub-menu>
-                  </el-sub-menu>
-                </el-menu>
-              </el-scrollbar>
-      </el-aside> -->
       <el-container>
-      <el-header>
-        <!-- <el-row :gutter="20">
-          <el-col :span="7" :offset="0"></el-col>
-          <el-col :span="10" :offset="0">
-            <el-card class="box-card">AI/GPT-3/text-davinci-003</el-card>
-          </el-col>
-          <el-col :span="7" :offset="0"></el-col>
-        </el-row> -->
-                    <el-card class="box-card">V0.0.1 AI/GPT-3/text-davinci-003</el-card>
-      </el-header>
-      
-      <el-main>
-        <el-card class="chat-card">
-                    
+        <el-header>
+          <el-card class="box-card">V0.0.1 AI/GPT-3/text-davinci-003</el-card>
+        </el-header>
 
-          <el-list>
-            <el-list-item v-for="message in messages" :key="message.id">
-              <el-row v-if="message.isMine" class="msg-row">
-                <el-col :span="1"> </el-col>
-                <el-col :span="22" style="display: flex; justify-content: flex-end">
-                  <v-md-preview class="msg" height="10px" :text="message.text"></v-md-preview>
-                </el-col>
-                <el-col :span="1" style="display: flex; justify-content: flex-end;">
-                  <el-avatar shape= "square" size="small" :src="userAvatarUrl" @error="errorHandler" />
-                </el-col>
-              </el-row>
-              <el-row v-else class="msg-row">
-                <el-col :span="1" style="display: flex; justify-content: flex-start;">
-                  <el-avatar shape= "square" size="small" :src="aiAvatarUrl" @error="errorHandler" />
-                </el-col>
-                <el-col :span="22" style="display: flex; justify-content: flex-start">
-                  <v-md-preview class="msg-ai" :text="message.text"></v-md-preview>
-                </el-col>
-                <el-col :span="1"> </el-col>
-              </el-row>
-            </el-list-item>
-          </el-list>
-        </el-card>
-      </el-main>
-      
-      <el-footer>
-        <el-form :model="form" class="msg-form" @submit.native.prevent>
-          <el-row :gutter="5">
-            <el-col :span="20">
-              <el-form-item>
-                <el-input class="chat-input" v-model="form.message" rows="2" maxlength="100" show-word-limit
-                  :disabled="disableForm" @keyup.enter.native="checkTextLen()" :placeholder="placeHolder" />
-              </el-form-item>
-            </el-col>
-            <el-col :span="4">
-              <el-form-item>
+        <el-main>
+          <el-card class="chat-card">
+            <el-list>
+              <el-list-item v-for="message in messages" :key="message.id">
+                <el-row v-if="message.isMine" class="msg-row">
+                  <el-col :span="1"> </el-col>
+                  <el-col
+                    :span="22"
+                    style="display: flex; justify-content: flex-end"
+                  >
+                    <v-md-preview
+                      class="msg"
+                      height="10px"
+                      :text="message.text"
+                    ></v-md-preview>
+                  </el-col>
+                  <el-col
+                    :span="1"
+                    style="display: flex; justify-content: flex-end"
+                  >
+                    <el-avatar
+                      shape="square"
+                      size="small"
+                      :src="userAvatarUrl"
+                      @error="errorHandler"
+                    />
+                  </el-col>
+                </el-row>
+                <el-row v-else class="msg-row">
+                  <el-col
+                    :span="1"
+                    style="display: flex; justify-content: flex-start"
+                  >
+                    <el-avatar
+                      shape="square"
+                      size="small"
+                      :src="aiAvatarUrl"
+                      @error="errorHandler"
+                    />
+                  </el-col>
+                  <el-col
+                    :span="22"
+                    style="display: flex; justify-content: flex-start"
+                  >
+                    <v-md-preview
+                      class="msg-ai"
+                      :text="message.text"
+                    ></v-md-preview>
+                  </el-col>
+                  <el-col :span="1"> </el-col>
+                </el-row>
+              </el-list-item>
+            </el-list>
+          </el-card>
+        </el-main>
+
+        <el-footer>
+          <el-form :model="form" class="msg-form" @submit.native.prevent>
+            <el-row :gutter="5">
+              <el-col :span="20">
+                <el-form-item>
+                  <el-input
+                    class="chat-input"
+                    v-model="form.message"
+                    rows="2"
+                    maxlength="100"
+                    show-word-limit
+                    :disabled="disableForm"
+                    @keyup.enter.native="checkTextLen()"
+                    :placeholder="placeHolder"
+                  />
+                </el-form-item>
+              </el-col>
+              <el-col :span="4">
+                <el-form-item>
                   <!-- <el-popover :visible="popoverVisible" placement="bottom" title="Title" :width="200"
 content="this is content, this is content, this is content">
                 <template #reference> -->
-                <el-button class="send-btn" @click="checkTextLen()" icon="Position" :disabled="disableForm"
-                  :loading="disableForm" color="#30b987a1" type="primary">{{ sendBtnText }}</el-button>
-<!-- </template>
+                  <el-button
+                    class="send-btn"
+                    @click="checkTextLen()"
+                    icon="Position"
+                    :disabled="disableForm"
+                    :loading="disableForm"
+                    color="#30b987a1"
+                    type="primary"
+                    >{{ sendBtnText }}</el-button
+                  >
+                  <!-- </template>
                 </el-popover> -->
-              </el-form-item>
-            </el-col>
-          </el-row>
-        </el-form>
-      </el-footer>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-footer>
       </el-container>
     </el-container>
     <el-message></el-message>
@@ -223,14 +197,16 @@ import { ElMessage } from "element-plus";
 export default {
   data() {
     return {
-      messages: [{
-        id: this.nextId,
-        isMine: false,
-        text: "我是openai大型自然语言ai模型gpt-3/text-davinci-003，可以回答您的任何问题，请您吩咐。",          
-      }],
+      messages: [
+        {
+          id: this.nextId,
+          isMine: false,
+          text: "我是openai大型自然语言ai模型gpt-3/text-davinci-003，可以回答您的任何问题，请您吩咐。",
+        },
+      ],
       resMessages: [],
       disableForm: false,
-      popoverVisible:true,
+      popoverVisible: true,
       sendBtnText: "发 送",
       placeHolder: "    给AI下达指令，描述越详细回复质量越高噢~",
       form: {
@@ -238,12 +214,11 @@ export default {
       },
       prompt: "",
       nextId: 1,
+      auth: import.meta.env.VITE_APP_OPENAI_AUTHORIZATION,
       userAvatarUrl:
         "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
-      // aiAvatarUrl:
-      //   "https://media.istockphoto.com/id/1460489368/vector/chatbot-logo-with-robot-head-chatbot-icon-artificial-intelligence-openai-openai-icon.jpg?b=1&s=170x170&k=20&c=vFpDX5gMY5MLAXxdSmSO8WCLaVivx89kPI_gG-YLlQg=",
       aiAvatarUrl:
-        "https://img1.baidu.com/it/u=2333513229,2298335749&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"
+        "https://img1.baidu.com/it/u=2333513229,2298335749&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
     };
   },
   methods: {
@@ -252,8 +227,12 @@ export default {
       this.prompt += "\nQ:你是谁";
       this.prompt += "\nA:";
       this.prompt += this.messages[0].text;
-      if (this.messages.length > 20){
-        for (let index = this.messages.length - 20 ; index < this.messages.length; index++) {
+      if (this.messages.length > 20) {
+        for (
+          let index = this.messages.length - 20;
+          index < this.messages.length;
+          index++
+        ) {
           if (this.messages[index].isMine) {
             this.prompt += "\nQ:";
             this.prompt += this.messages[index].text;
@@ -265,7 +244,7 @@ export default {
       } else {
         for (let index = 0; index < this.messages.length; index++) {
           if (index === 0) {
-            continue
+            continue;
           }
           if (this.messages[index].isMine) {
             this.prompt += "\nQ:";
@@ -274,10 +253,10 @@ export default {
             this.prompt += "\nA:";
             this.prompt += this.messages[index].text;
           }
-        }        
+        }
       }
 
-      console.log(this.prompt)        
+      console.log(this.prompt);
     },
     checkTextLen() {
       if (this.form.message.trim().length === 0) {
@@ -318,8 +297,7 @@ export default {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            " Bearer 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",
+          Authorization: " Bearer " + this.auth,
         },
         body: JSON.stringify({
           model: "text-davinci-003",
@@ -329,9 +307,7 @@ export default {
           top_p: 1,
           frequency_penalty: 0.0,
           presence_penalty: 0.0,
-          stop: [" Human:", " AI:"]
-
-          //,          stop: ["\n"]
+          stop: [" Human:", " AI:"],
         }),
       })
         .then((response) => response.json())
@@ -356,18 +332,6 @@ export default {
         .finally(() => {
           this.enableFormComp();
         });
-
-      //       fetch("https://api.openai.com/v1/embeddings", {
-      //   method: "POST",
-      //   headers: { 'Content-Type': 'application/json', 'Authorization':' Bearer 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' },
-      //   body: JSON.stringify({
-      //    input : "Your text string goes here",
-      //    model : "text-embedding-ada-002"
-      //   })
-      // })
-      //   .then(response => response.json())
-      //   .then(data => console.log(data))
-      //   .catch(error => ERR);
       this.form.message = "";
     },
   },
@@ -380,9 +344,6 @@ export default {
             x.scrollTop = x.scrollHeight;
           }, 0);
         });
-
-        // let x = document.getElementsByClassName("chat-card")[0];
-        // x.scrollTop = x.scrollHeight;
       },
       deep: true,
     },
